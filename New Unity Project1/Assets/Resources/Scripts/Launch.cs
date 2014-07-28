e53116b5b5564141f71f0738d208ee33595648d3
cloud.z <xmzyl@hotmail.com>
2014-07-28 20:47:07 +0800

    update

---
+++ New Unity Project1/Assets/Resources/Scripts/Launch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (loginUI_ == null)
+            loginUI_ = Resources.Load("UI/LoginUI") as Canvas;
+	}
+
+    public Canvas loginUI_ = null;
+}
---
+++ New Unity Project1/Assets/Resources/Scripts/ballControllor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballControllor : MonoBehaviour {
+
+    GameObject sun1;
+    GameObject sun2;
+
+    bool flagUp = true;
+    float initAngle = 180;
+
+    float angleSpeed = 4;
+    float eulerAngle = 0;
+    void Awake() {
+    }
+	// Use this for initialization
+	void Start () {
+        sun1 = GameObject.FindGameObjectWithTag("sun");
+        sun2 = GameObject.FindGameObjectWithTag("sun2");
+
+
+
+        transform.parent = sun1.transform;
+	}
+
+    void FixedUpdate() {
+        if (flagUp) {
+            sun1.transform.Rotate(new Vector3(0, 0, 1), angleSpeed);
+            eulerAngle += angleSpeed;
+			if ((initAngle + eulerAngle) >= 360) {
+                transform.parent = sun2.transform;
+                flagUp = false;
+                eulerAngle = 0;
+                initAngle = 0;
+                sun1.transform.Rotate(new Vector3(0, 0, 0));
+                return;
+            }
+        }
+
+
+        if (!flagUp) {
+            sun2.transform.Rotate(new Vector3(0, 0, -1), angleSpeed);
+            eulerAngle += angleSpeed;
+			if (eulerAngle >= 360) {
+                transform.parent = sun1.transform;
+                flagUp = true;
+                eulerAngle = 0;
+                sun1.transform.Rotate(new Vector3(0, 0, 0));
+                return;
+            }
+        }
+    }
+	
+	void Update () {
+
+	}
+}
